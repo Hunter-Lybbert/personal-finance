b6f1250ba00786d67dfc2581f0d906885f1a69c8
Hunter Lybbert <hunterdl@byu.edu>
2022-11-23 10:13:21 -0500

    Added pre-commit

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ Analysis of Personal Finances
 * Analyze and summarize
 
 Follow [these instructions](https://towardsdatascience.com/how-to-access-google-sheet-data-using-the-python-api-and-convert-to-pandas-dataframe-5ec020564f0e)  to allow access to your google sheets.
-
--- src/common/google_api_helpers.py
+++ src/common/google_api_helpers.py
@@ -13,21 +13,18 @@ def get_google_creds() -> None:
 
     :return: None
     """
-    scopes = 'https://www.googleapis.com/auth/spreadsheets'
+    scopes = "https://www.googleapis.com/auth/spreadsheets"
 
     # Setup the Sheets API
-    store = file.Storage('credentials.json')
+    store = file.Storage("credentials.json")
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_secret.json', scopes)
+        flow = client.flow_from_clientsecrets("client_secret.json", scopes)
         creds = tools.run_flow(flow, store)
-    service = build('sheets', 'v4', http=creds.authorize(Http()))
+    service = build("sheets", "v4", http=creds.authorize(Http()))
 
 
-def get_google_sheet(
-    spreadsheet_id: str,
-    range_name: str
-) -> Any:
+def get_google_sheet(spreadsheet_id: str, range_name: str) -> Any:
     """
     Retrieve sheet data using OAuth credentials and Google Python API.
 
@@ -36,24 +33,28 @@ def get_google_sheet(
 
     :returns: Some type of API result about sheets
     """
-    scopes = 'https://www.googleapis.com/auth/spreadsheets'
+    scopes = "https://www.googleapis.com/auth/spreadsheets"
 
     # Setup the Sheets API
-    store = file.Storage('credentials.json')
+    store = file.Storage("credentials.json")
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_secret.json', scopes)
+        flow = client.flow_from_clientsecrets("client_secret.json", scopes)
         creds = tools.run_flow(flow, store)
-    service = build('sheets', 'v4', http=creds.authorize(Http()))
+    service = build("sheets", "v4", http=creds.authorize(Http()))
 
     # Call the Sheets API
-    gsheet = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
+    gsheet = (
+        service.spreadsheets()
+        .values()
+        .get(spreadsheetId=spreadsheet_id, range=range_name)
+        .execute()
+    )
     return gsheet
 
 
-
 def gsheet2df(gsheet):
-    """ Converts Google sheet data to a Pandas DataFrame.
+    """Converts Google sheet data to a Pandas DataFrame.
     Note: This script assumes that your data contains a header file on the first row!
 
     Also note that the Google API returns 'none' from empty cells - in order for the code
@@ -61,10 +62,10 @@ def gsheet2df(gsheet):
     or update the code to account for such instances.
 
     """
-    header = gsheet.get('values', [])[0]   # Assumes first line is header!
-    values = gsheet.get('values', [])[1:]  # Everything else is data.
+    header = gsheet.get("values", [])[0]  # Assumes first line is header!
+    values = gsheet.get("values", [])[1:]  # Everything else is data.
     if not values:
-        print('No data found.')
+        print("No data found.")
     else:
         all_data = []
         for col_id, col_name in enumerate(header):
